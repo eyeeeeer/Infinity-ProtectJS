ac0021cfe918c7550859b409f179e0409cd5eaa6
ProBAN <87663703+ProBANgh@users.noreply.github.com>
2022-05-18 19:20:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,126 @@
+const { MessageEmbed, Client, Collection, Intents } = require("discord.js");
+const keep_alive = require('./keep-alive.js')
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_WEBHOOKS]
+});
+
+const fs = require('node:fs')
+const mongoose = require('mongoose')
+var configs = require('./serverModel.js')
+mongoose.connect('mongodb+srv://admin:analforzel@cluster0.klsbg.mongodb.net/InfinityProtect?retryWrites=true&w=majority');
+
+
+const yaml = require('js-yaml');
+
+
+const discordModals = require('discord-modals');
+
+const config = yaml.load(fs.readFileSync('./config.yml', 'utf8'));
+client.config = config
+
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const data = []
+
+for (const file of commandFiles) {
+    const commandfile = require(`./commands/${file}`);
+  
+    client.commands.set(commandfile.name, commandfile);
+    data.push({
+        name: commandfile.name,
+        description: commandfile.description,
+        options: commandfile.options,
+        default_permission: commandfile.default_permission,
+        permissions: commandfile.permissions
+    });
+}
+
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+client.on('guildCreate', async (guild) => {
+  await client.application.commands.set(data);
+  var guildData = new configs({ _id: guild.id, antiNuke: true, channelDelete: {count: 4, mode: true}, channelCreate: {count: 10, mode: true}, channelUpdate: {count: 5, mode: true}, roleCreate: {count: 10, mode: true}, roleDelete: {count: 4, mode: true}, roleUpdate: {count: 3, mode: true}, webhookCreate: {count: 2, mode: true}, blockNewAccounts: false, blockNewAccountsTime: "3d", wl: []});
+      guildData.save(function (err, data) {
+       if (err) return  console.error(err);
+       });
+  
+       var cnl = client.channels.cache.get('964508338530447430')
+       
+       const gdEmbed = new MessageEmbed()
+       .setTitle('Новый сервер')
+       .setDescription(`${guild.name}\nВладелец: ${guild.ownerId}\nID: ${guild.id}`)
+       .setColor('#5865F2')
+       cnl.send({embeds: [gdEmbed]})
+       
+
+
+
+})
+client.on("debug", ( e ) => console.log(e))
+client.on('interactionCreate', async (interaction) => {
+    if (interaction.isCommand()) {
+            if (!interaction.client.commands.has(interaction.commandName)) return;
+            
+            try {
+                await interaction.client.commands.get(interaction.commandName).execute(interaction.client, interaction, {
+                  aembed: aembed,
+                  colors: {
+                    default: "#5865F2",
+                    error: "RED"
+                  }
+                });
+            } catch (error) {
+                console.error(error);
+
+                return interaction.reply({
+                    content: 'Произошла неизвестная ошибка при выполнении команды'
+                });
+}
+    
+      
+    };
+})
+
+let aembed = (rustitle, rusdesc, color) => {
+  let rusik = rustitle.toLowerCase()
+  if(rusik == "ошибка") rustitle = ":x: | Ошибка"
+  if(rusik == "успешно") rustitle = ":white_check_mark: | Успешно"
+
+  const Embed = new MessageEmbed()
+  .setTitle(rustitle)
+  .setDescription(rusdesc).setColor(color).setFooter({text: "Infinity Protect"}).setTimestamp()
+
+  return Embed
+}
+
+client.on("ready", async () => {
+  console.log('Начинаю процесс обновления команд...');
+    //await configs.deleteMany({})
+    await client.application.commands.set(data);
+    const aGuilds = client.guilds.cache.map(guild => guild.id);
+    for (var g of aGuilds) {
+      //console.log(g)/
+      var guildData = new configs({ _id: g, antiNuke: true, channelDelete: {count: 4, mode: true}, channelCreate: {count: 10, mode: true}, channelUpdate: {count: 5, mode: true}, roleCreate: {count: 10, mode: true}, roleDelete: {count: 4, mode: true}, roleUpdate: {count: 3, mode: true}, webhookCreate: {count: 2, mode: true}, blockNewAccounts: false, blockNewAccountsTime: "3d", wl: []});
+      guildData.save(function (err, data) {
+       if (err)  console.error('err');
+       });
+
+    }
+
+});
+
+
+
+console.log('Бот запускается...')
+client.login("OTY0NTA0NzQxMjIyNjc4NTc5.YllnCQ.CkusTZIi8dserCwVYF_etIa3Lr0")
+console.log('какого хрена') //rate limits //а ошибка где //nety // за два часа должно было пофиксится //такое уже было, ждём
---
+++ serverModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+require('mongoose-long')(mongoose);
+const {Types: {Long}} = mongoose
+
+//let con = mongoose.connect('mongodb+srv://admin:analforzel@cluster0.klsbg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+
+//module.exports.db = con 
+
+var guildSchema = new mongoose.Schema({
+    _id: String,
+    antiNuke: Boolean,
+    channelDelete: {count: Number, mode: Boolean},
+    channelCreate: {count: Number, mode: Boolean},
+    channelUpdate: {count: Number, mode: Boolean},    
+    roleDelete: {count: Number, mode: Boolean},
+    roleCreate: {count: Number, mode: Boolean},
+    roleUpdate: {count: Number, mode: Boolean},
+    webhookCreate: {count: Number, mode: Boolean},
+    blockNewAccounts: Boolean,
+    blockNewAccountsTime: String,
+    wl: [ String ]
+});
+
+
+module.exports = mongoose.model("configs", guildSchema);
