5b77e77e58c999544a800e47482589755be8a366
ProBAN <87663703+ProBANgh@users.noreply.github.com>
2022-06-26 20:27:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { ShardingManager } = require('discord.js');
 
 const manager = new ShardingManager('./bot.js', { 
-  token: "OTY0NTA0NzQxMjIyNjc4NTc5.GnldBP.YAeEZYJpnjHghkD2298_qDmvAhihQbbQVvMywo",
+  token: "OTY0NTA0NzQxMjIyNjc4NTc5.G_ro9W.TgdDHPRJxQQvV7A0UlZCXXNs-a2QfekeibB700",
   totalShards: "auto"
 });
 
