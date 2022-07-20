280c4f06860b46dde34615ed3e0a4b6bca54463a
ProBAN <87663703+ProBANgh@users.noreply.github.com>
2022-07-20 23:47:44 +0300

    Update banModel.js

--- banModel.js
+++ banModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const con = mongoose.connect('mongodb+srv://admin:analforzel@cluster0.klsbg.mongodb.net/InfinityProtect?retryWrites=true&w=majority')
+const con = mongoose.connect('mongodb+srv://admin:BdJBm8ZBY7uneonw@cluster0.klsbg.mongodb.net/InfinityProtect?retryWrites=true&w=majority')
 
 module.exports.db = con
 
